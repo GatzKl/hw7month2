39118ae8feb039e2fda0b31051e4e27b1619af90
Alexandr <74298908+Alexandr205@users.noreply.github.com>
2022-06-28 12:01:35 +0600

    Initial Commit

---
+++ hw7month2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw7month2
+//
+//  Created by Александр Калашников on 28/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
